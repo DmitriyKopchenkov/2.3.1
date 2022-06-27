744a7051ac8a6ef8c3d3ca03c8f02bf646424a95
DmitriyKopchenkov <103284097+DmitriyKopchenkov@users.noreply.github.com>
2022-06-27 13:58:46 +0300

    commit

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -22,31 +22,27 @@ public class UserServiceImp implements UserService {
         this.userDao = userDao;
     }
 
-    @Transactional(readOnly = true)
+
     public void add(User user) {
 
         userDao.add(user);
     }
 
-    @Transactional(readOnly = true)
     public List<User> index() {
 
         return userDao.index();
     }
 
-    @Transactional(readOnly = true)
     public User show(long id) {
 
         return userDao.show(id);
     }
 
-    @Transactional(readOnly = true)
     public void update(User updatedUser) {
 
         userDao.update(updatedUser);
     }
 
-    @Transactional(readOnly = true)
     public void delete(long id) {
 
         userDao.delete(id);
